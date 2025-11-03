672938173fe856248dc2e45de82acbc5e160c052
mohit jain <mohitjain1311@gmail.com>
2025-11-03 15:28:30 +1100

    chore: adding open api spec swagger generation utility

--- ticket-service/src/main/java/com/ticketing/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/ticketing/TicketServiceApplication.java
@@ -1,11 +1,20 @@
 package com.ticketing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Ticket Pricing Service API",
+                version = "1.0.0",
+                description = "API documentation for the Ticket Service, part of the Movie Ticket System"
+        )
+)
 public class TicketServiceApplication {
 
     public static void main(String[] args) {
